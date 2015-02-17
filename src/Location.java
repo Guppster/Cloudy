eba10a7d748501a0f93354fe5470528bf3df7c84
Guppster <guppst3r@gmail.com>
2015-02-16 22:14:30 -0500

    Implemented Location Objects

--- src/Location.java
+++ src/Location.java
@@ -3,5 +3,76 @@
  */
 public class Location 
 {
+    private String name;
+    private TermObject shortTerm;
+    private TermObject longTerm;
+    private TermObject currentTerm;
     
-}
+    public Location(String name, TermObject shortTerm, TermObject longTerm, TermObject currentTerm)
+    {
+        this.name = name;
+        this.shortTerm = shortTerm;
+        this.longTerm = longTerm;
+        this.currentTerm = currentTerm;
+    }//End of specific constructor
+    
+    public Location(String name)
+    {
+        this.name = name;
+    }//End of general constructor
+    
+    public String getName()
+    {
+        return "";
+    }
+    
+    public TermObject getShortTerm()
+    {
+        return new TermObject() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        };
+    }
+
+    public TermObject getLongTerm()
+    {
+        return new TermObject() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        };
+    }
+
+    public TermObject getCurrentTerm()
+    {
+        return new TermObject() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        };
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public void setShortTerm(TermObject shortTerm) 
+    {
+        this.shortTerm = shortTerm;
+    }
+
+    public void setLongTerm(TermObject longTerm) 
+    {
+        this.longTerm = longTerm;
+    }
+
+    public void setCurrentTerm(TermObject currentTerm) 
+    {
+        this.currentTerm = currentTerm;
+    }
+}//End of Location class
--- src/LocationList.java
+++ src/LocationList.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+
 /**
  * @author: Gurpreet
  */
 public class LocationList 
 {
+    private ArrayList<Location> regions;
     
-}
+    public LocationList()
+    {
+        regions = new ArrayList<Location>();
+    }//End of general constructor
+    
+    public LocationList(ArrayList<Location> regions)
+    {
+        this.regions = regions;
+    }//End of specific constructor
+    
+    public Location searchList(String name)
+    {
+        return new Location();
+    }
+    
+    public boolean isLocationInList(Location location)
+    {
+        return true;
+    }
+    
+    public void addRegion(Location location)
+    {
+        
+    }
+    
+    public void deleteRegion(Location location)
+    {
+        
+    }
+    
+    public ArrayList<Location> getLocationList()
+    {
+        return regions;
+    }
+}//End of LocationList
