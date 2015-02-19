59d30c1a12385bda4d94ddff1bba3c006c9ceba8
Guppster <guppst3r@gmail.com>
2015-02-18 19:26:24 -0500

    finished parser abstract class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,23 @@
+import com.squareup.okhttp.OkHttpClient;
+import org.json.JSONObject;
+
 /**
  * @author: Gurpreet
  */
 public abstract class Parser 
 {
+    protected OkHttpClient client;
+    protected JSONObject dataRaw;
+    protected BaseData dataFormatted;
     
-}
+    protected Parser()
+    {
+        client = new OkHttpClient();
+        dataRaw = new JSONObject();
+    }
+    
+    protected abstract TermObject parse();
+    
+    protected abstract void fetchForecast(String str);
+
+}//End of parser abstract class
