403deb58e6ee8fdc8d05f2699e5864578e999583
Guppster <guppst3r@gmail.com>
2015-02-26 21:55:58 -0500

    Finished saving and loading in configuration

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,8 +1,15 @@
+import sun.security.util.BitArray;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.prefs.Preferences;
+
 /**
  * @author: Gurpreet
  */
 public class Configuration 
 {
+    private Preferences prefs;
     private LocationList locations;
     private tempUnits degrees;
     private boolean[] viewObject;
@@ -15,6 +22,7 @@ public class Configuration
      */
     public Configuration(LocationList locations, tempUnits degrees, boolean[] viewObject)
     {
+        prefs = Preferences.userRoot().node(this.getClass().getName());
         this.locations = locations;
         this.degrees = degrees;
         this.viewObject = viewObject;
@@ -78,5 +86,97 @@ public class Configuration
     public void setDegrees(tempUnits degrees)
     {
         this.degrees = degrees;
+    }
+    
+    public boolean save()
+    {
+        try
+        {
+            prefs.putByteArray("locations", locationsToByteArray());
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        prefs.put("tempUnits", degrees.name());
+        
+        prefs.putByteArray("viewableObjects", viewableObjectsToByteArray());
+        
+        return true;
+    }
+    
+    public boolean load()
+    {
+        return false;
+    }
+
+    /**
+     * * 
+     * @return
+     * @throws IOException
+     */
+    private byte[] locationsToByteArray() throws IOException
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(baos);
+        
+        for (Location element : locations.getLocationList()) 
+        {
+            out.writeUTF(element.getName());
+        }
+        
+        return baos.toByteArray();
+    }
+
+    /**
+     * * 
+     * @param bytes
+     * @return
+     * @throws IOException
+     */
+    private ArrayList<Location> byteArrayToLocations(byte[] bytes) throws IOException
+    {
+        ArrayList<Location> arr = new ArrayList<Location>();
+        
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        DataInputStream in = new DataInputStream(bais);
+        
+        while (in.available() > 0)
+        {
+            arr.add(new Location(in.readUTF()));
+        }
+        
+        return arr;
+    }
+
+    /**
+     * * 
+     * @return
+     */
+    private byte[] viewableObjectsToByteArray()
+    {
+        BitArray bits = new BitArray(viewObject);
+        return bits.toByteArray();
+    }
+
+    /**
+     * *
+     * @param bytes
+     * @return
+     */
+    private boolean[] byteArrayToBoolean(byte[] bytes)
+    {
+        boolean[] bits = new boolean[bytes.length * 8];
+        
+        for (int i = 0; i < bytes.length * 8; i++) 
+        {
+            if ((bytes[i / 8] & (1 << (7 - (i % 8)))) > 0)
+            {
+                bits[i] = true;
+            }
+        }
+        
+        return bits;
     }
 }//End of Configuration
