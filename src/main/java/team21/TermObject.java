2485fa2e3ccf8f62442c2792bccf30272d1597bf
Ke Xin Xu <kxu72@uwo.ca>
2015-03-04 14:11:42 -0500

    comment

--- src/main/java/team21/TermObject.java
+++ src/main/java/team21/TermObject.java
@@ -17,6 +17,12 @@ public abstract class TermObject
 
     }//End of constructor
 
-    //Fill in data field with a specific type of BaseData, this changes the type of BaseData to the specific type. Each TermObject has to implement this method for its specfic data.
+
+    /**
+     * Fill in data field with a specific type of BaseData,
+     * this changes the type of BaseData to the specific type.
+     * Each TermObject has to implement this method for its specific data.
+     */
     protected abstract void parseTermData();
+
 }//End of TermObject abstract class
