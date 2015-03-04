04651c0d5f22a13d118826769904413a03d226b1
Ke Xin Xu <kxu72@uwo.ca>
2015-03-04 10:59:28 -0500

    comment

--- src/main/java/team21/TermObject.java
+++ src/main/java/team21/TermObject.java
@@ -1,15 +1,20 @@
 package team21;
 
 /**
+ * Abstract Class storing an array of classes, each of which store the specific data at a given time or day
+ *
  * @author: Gurpreet
  */
-public abstract class TermObject 
+public abstract class TermObject
 {
+    ///// Attribute //////
     protected BaseData[] data;
 
+    ///// Constructor /////
     protected TermObject(int size)
     {
         data = new BaseData[size];
+
     }//End of constructor
 
     //Fill in data field with a specific type of BaseData, this changes the type of BaseData to the specific type. Each TermObject has to implement this method for its specfic data.
