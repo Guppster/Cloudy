dad9b4972ca4e50732dc4a72d517993163820b47
Ke Xin Xu <kxu72@uwo.ca>
2015-03-07 13:23:42 -0500

    edited tempValues: class to help obtain information needed to display on GUI

--- src/main/java/team21/TempValues.java
+++ src/main/java/team21/TempValues.java
@@ -6,51 +6,71 @@ import java.util.Scanner;
  * Created by Christ Xu on 2015-03-06.
  */
 public class TempValues
+{
 
+    public void main(String[] args)
+    {
+        Main app = new Main();
+        LocationList locations = app.getLocations();
+        Configuration config = app.getConfig();
+        Scanner read = new Scanner(System.in);
 
-        public void main(String[] args)
+        //search location (user input)
+        String input = read.nextLine();
+
+        //convert to the offical name
+        CurrentTermParser ctParse = new CurrentTermParser(input);
+        String officalName; ///NEED HELP?
+
+        //Access Current Data (as long as you have the "official" name of the location)
+        Location local = locations.searchList(officalName);
+        if (local != null)
         {
-            Main app = new Main();
-            LocationList locations = app.getLocations();
-            Configuration config = app.getConfig();
-            Scanner read = new Scanner(System.in);
+            //convert data to usable form (icon and description is already string, so extraction is all that's needed)
 
-            //search location
-            String input = read.nextLine();
+            //Current Term
+            String ctHighLow = local.getCurrentTerm().data[0].getTempMin() + "\t " + local.getCurrentTerm().data[0].getTempMax();
+            String ctCurrentTemp = Double.toString(local.getCurrentTerm().data[0].getTemp());
+            String ctWind = "Wind: " + local.getCurrentTerm().data[0].getWindSpeed() + " " + local.getCurrentTerm().data[0].getWindDirection();
+            String ctAir = "Air Pressure: " + local.getCurrentTerm().data[0].getPressure();
+            String ctIconID = local.getCurrentTerm().data[0].getIconID();
+            String ctDescription = local.getCurrentTerm().data[0].getDescription();
 
-            ////////STUCK!
+            //Long Term
+            String[] ltHighLow = new String[5];
+            String[] ltCurrentTemp = new String[5];
+            String[] ltIconID = new String[5];
+            String[] ltDescription = new String[5];
 
-
-            //convert to the offical name
-            String name = "";
-            CurrentTermParser ltParse = new CurrentTermParser(name);
-
-
-            //Access Current Data
-            Location local = locations.searchList(name);
-            if (local != null)
+            // in each array of 5, store the respective information.
+            for (int i = 0; i < 5; i++)
             {
-                CurrentTerm[] ct = local.getCurrentTerm();
-                 double temp;
-                 double tempMin;
-                 double tempMax;
-                 double windSpeed;
-                 int windDirection;
-                 double pressure;
-
-                //convert data to usable form (icon and description is already string, so extraction is all that's needed)
-                String highLow = tempMin + "\t "+ tempMax;
-                String currentTemp = temp;
-                String wind = "Wind: " +windSpeed  + " "+ windDirection;
-                String air = "Air Pressure: " + pressure;
-                String iconID;
-                String description;
-
-
-
+                ltHighLow[i] = local.getLongTerm().data[i].getTempMin() + "\t " + local.getLongTerm().data[i].getTempMax();
+                ltCurrentTemp[i] = Double.toString(local.getLongTerm().data[i].getTemp());
+                ltIconID[i] = local.getLongTerm().data[i].getIconID();
+                ltDescription[i] = local.getLongTerm().data[i].getDescription();
             }
 
+
+            //Short Term
+            String[] stCurrentTemp = new String[8];
+            String[] stIconID = new String[8];
+            String[] stDescription = new String[8];
+
+            // in each array of 5, store the respective information.
+            for (int i = 0; i < 5; i++)
+            {
+                stCurrentTemp[i] = Double.toString(local.getShortTerm().data[i].getTemp());
+                stIconID[i] = local.getLongTerm().data[i].getIconID();
+                stDescription[i] = local.getLongTerm().data[i].getDescription();
+            }
+
+            //Mars data ?
+
+
         }
+
+    }
 
 }
 
