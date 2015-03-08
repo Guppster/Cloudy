b043cf4f27f4a4177e521436d445f8ee951c2208
Ke Xin Xu <kxu72@uwo.ca>
2015-03-08 00:12:30 -0500

    javadoc Comment for all class labelled DATA

--- src/main/java/team21/LongTermData.java
+++ src/main/java/team21/LongTermData.java
@@ -7,24 +7,24 @@ package team21;
  */
 public class LongTermData extends BaseData
 {
-    /**** Fields ****/
     private double tempNight; //temperature through the night
     private double tempEve; //temperature through the evening
     private double tempMorn; //temperature in the morning
 
     /**
+     * Constructor initialize the attributes with the first three parameters and sends the rest to the super class
      *
-     * @param tempNight
-     * @param tempEve
-     * @param tempMorn
-     * @param temp
-     * @param tempMin
-     * @param tempMax
-     * @param windSpeed
-     * @param windDirection
-     * @param pressure
-     * @param iconID
-     * @param description
+     * @param tempNight     the temperature at night
+     * @param tempEve       the temperature during the evening
+     * @param tempMorn      the temperature during the morning
+     * @param temp          the current temperature
+     * @param tempMin       the minimum predicted temperature
+     * @param tempMax       the maximum predicted temperature
+     * @param windSpeed     the expected wind speed
+     * @param windDirection the direction the wind blows
+     * @param pressure      the current air pressure
+     * @param iconID        the string representing the icon that will be used to visually display the current weather
+     * @param description   a short description of the weather
      */
     public LongTermData(double tempNight, double tempEve, double tempMorn, double temp, double tempMin, double tempMax, double windSpeed, int windDirection, double pressure, String iconID, String description)
     {
@@ -34,33 +34,61 @@ public class LongTermData extends BaseData
         this.tempMorn = tempMorn;
     }//End of constructor
 
-    ///// Getters and Setters /////
-
+    /**
+     * Extract the temperature at night
+     *
+     * @return the temperature at night
+     */
     public double getTempNight()
     {
         return tempNight;
     }
 
+    /**
+     * Updates the temperature at night
+     *
+     * @param tempNight the new temperature at night
+     */
     public void setTempNight(double tempNight)
     {
         this.tempNight = tempNight;
     }
 
+    /**
+     * Extract the temperature during the evening
+     *
+     * @return the temperature during the evening
+     */
     public double getTempEve()
     {
         return tempEve;
     }
 
+    /**
+     * Update the temperature during the evening
+     *
+     * @param tempEve the new temperature during the evening
+     */
     public void setTempEve(double tempEve)
     {
         this.tempEve = tempEve;
     }
 
+    /**
+     * Extract  the temperature during the morning
+     *
+     * @return the temperature during the morning
+     */
     public double getTempMorn()
     {
         return tempMorn;
     }
 
+    /**
+     * Update  the temperature during the morning
+     *
+     * @param tempMorn the new temperature during the morning
+     */
     public void setTempMorn(double tempMorn)
     {
         this.tempMorn = tempMorn;
--- src/main/java/team21/ShortTermData.java
+++ src/main/java/team21/ShortTermData.java
@@ -7,20 +7,20 @@ package team21;
  */
 public class ShortTermData extends BaseData
 {
-    /**** Fields ****/
     private String time;         // time of day in which these current weather value is valid for
 
     /**
-     * 
-     * @param time
-     * @param temp
-     * @param tempMin
-     * @param tempMax
-     * @param windSpeed
-     * @param windDirection
-     * @param pressure
-     * @param iconID
-     * @param description
+     * Constructor initialize the attribute with the first parameter and sends the rest to the super class
+     *
+     * @param time          the time at which for the other attributes are true
+     * @param temp          the current temperature
+     * @param tempMin       the minimum predicted temperature
+     * @param tempMax       the maximum predicted temperature
+     * @param windSpeed     the expected wind speed
+     * @param windDirection the direction the wind blows
+     * @param pressure      the current air pressure
+     * @param iconID        the string representing the icon that will be used to visually display the current weather
+     * @param description   a short description of the weather
      */
     public ShortTermData(String time, double temp, double tempMin, double tempMax, double windSpeed, int windDirection, double pressure, String iconID, String description)
     {
@@ -28,13 +28,21 @@ public class ShortTermData extends BaseData
         this.time = time;
     }//End of constructor
 
-    ///// Getters and Setters /////
-
+    /**
+     * Extract the  time
+     *
+     * @return the time
+     */
     public String getTime()
     {
         return time;
     }
 
+    /**
+     * Update the time
+     *
+     * @param time the new time
+     */
     public void setTime(String time)
     {
         this.time = time;
