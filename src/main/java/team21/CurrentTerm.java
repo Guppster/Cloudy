f1ed9229415055cf798539266fa0c36c7e60a395
Guppster <guppst3r@gmail.com>
2015-03-07 21:14:54 -0500

    Fixed CurrentTerm (reverted)

--- src/main/java/team21/CurrentTerm.java
+++ src/main/java/team21/CurrentTerm.java
@@ -1,71 +1,35 @@
 package team21;
 
 /**
- * Class storing the current weather data. Extends BaseData and have additional attributes, precipitation, sunrise, and sunset
+ + * Class extending TermObject to store 1 CurrentTermData (TermObject used for consistency)
  *
  * @author: Gurpreet
  */
-
-public class CurrentTermData extends BaseData
+public class CurrentTerm extends TermObject
 {
+    /**
+     * *Fields***
+     */
+    private CurrentTermData[] Cdata;
 
-    /**** Fields ****/
-    private int precipitation; //level or precipitation
-    private int sunrise;//time of expected sunrise
-    private int sunset;//time of expected sunset
+    /**
+     * @param Cdata
+     */
+    public CurrentTerm(CurrentTermData[] Cdata)
+    {
+        super(1);
+        this.Cdata = Cdata;
+        parseTermData();
+    }//End of constructor
 
     /**
      *
-     * @param precipitation
-     * @param sunrise
-     * @param sunset
-     * @param temp
-     * @param tempMin
-     * @param tempMax
-     * @param windSpeed
-     * @param windDirection
-     * @param pressure
-     * @param iconID
-     * @param description
      */
-    public CurrentTermData(int precipitation, int sunrise, int sunset, double temp, double tempMin, double tempMax, double windSpeed, int windDirection, double pressure, String iconID, String description)
+    protected void parseTermData()
     {
-        super(temp, tempMin, tempMax, windSpeed, windDirection, pressure, iconID, description);
-        this.precipitation = precipitation;
-        this.sunrise = sunrise;
-        this.sunset = sunset;
-    }
-
-
-    /**** Getters and Setters ****/
-
-    public int getPrecipitation()
-    {
-        return precipitation;
-    }
-
-    public void setPrecipitation(int precipitation)
-    {
-        this.precipitation = precipitation;
-    }
-
-    public int getSunrise()
-    {
-        return sunrise;
-    }
-
-    public void setSunrise(int sunrise)
-    {
-        this.sunrise = sunrise;
-    }
-
-    public int getSunset()
-    {
-        return sunset;
-    }
-
-    public void setSunset(int sunset)
-    {
-        this.sunset = sunset;
+        for (int i = 0; i < data.length; i++)
+        {
+            data[i] = Cdata[i];
+        }
     }
 }
