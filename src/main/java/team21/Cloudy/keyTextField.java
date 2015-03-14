ce880ca573cef3695d475344110613c4a0ef9e65
Guppster <guppst3r@gmail.com>
2015-03-14 05:33:24 -0400

    Fixed invalid resource paths

--- src/main/java/team21/Cloudy/keyTextField.java
+++ src/main/java/team21/Cloudy/keyTextField.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JTextField;
@@ -76,7 +76,8 @@ public class keyTextField extends KeyAdapter
     {
         try
         {
-            BufferedReader in = new BufferedReader(new FileReader(keyTextField.class.getResource("/city_list_clean.txt").getPath()));
+            System.out.println(keyTextField.class.getResource("/other/city_list_clean.txt").getPath());
+            BufferedReader in = new BufferedReader(new InputStreamReader(keyTextField.class.getResourceAsStream("/other/city_list_clean.txt")));
             String str;
 
             while ((str = in.readLine()) != null)
